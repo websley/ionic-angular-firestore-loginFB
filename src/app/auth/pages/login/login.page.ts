79642a4b33dc46239935565efd478d5127b1ef0d
Wesley <wesley.s.gomes@hotmail.com>
2020-11-21 15:40:33 -0300

    layout, formulario e validações da tela de login/cadastro

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'login',
+    actionChange: 'Create account'
+  };
 
-  ngOnInit() {
+  private nameControl= new FormControl('',[Validators.required, Validators.minLength(3)])
+
+  constructor(
+    private fb: FormBuilder
+    ) { }
+
+  ngOnInit(): void {
+    this.creatForm();
+  }
+
+  private creatForm(){
+    this.authForm = this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  get email(){
+    return this.authForm.get('email');
+  }
+
+  get password(){
+    return this.authForm.get('password');
+  }
+
+  get name(){
+    return this.authForm.get('name');
+  }
+
+  changeAuthAction(){
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn ? 'Create accont' : 'Ja possuo cadastro';
+    !isSignIn 
+      ? this.authForm.addControl('name', this.nameControl) 
+      : this.authForm.removeControl('name');
+  }
+
+  onSubmit(){
+    console.log(this.authForm.value);
   }
 
 }
